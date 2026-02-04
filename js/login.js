6e24ec3222a953b7a42ad388b14beae8f997c838
mazdim131 <syandanadimas7.@gmail.com>
2026-02-04 19:48:16 +0700

    change username & Password

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ function redirect() {
     let username_log = document.getElementById("username").value;
     let password_log = document.getElementById("password").value;
 
-    if (username_log == "admin" && password_log == "123") {
+    if (username_log == "PPLGX3" && password_log == "JAYA_123") {
         alert("Login berhasil!")
         window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeh8flM6s8AvxULXIMNFY9SGqqRA_Ka4q4AfpsPjJ8jSIh9wA/viewform"
     } else {
